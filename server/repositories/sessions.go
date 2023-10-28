fbc76ba190bd0856e2b9643075bfcc035881456c
TheWisePigeon <zozozozeph@gmail.com>
2023-10-28 19:31:34 +0000

    relocate code

--- server/repositories/sessions.go
+++
@@ -1,8 +0,0 @@
-package repositories
-
-import "cold/models"
-
-func CreateSession(session_id string, user_id string) error {
-	_, err := DB.NamedExec("insert into sessions(id, session_user) values(:id, :session_user)", &models.Session{Id: session_id, SessionUser: user_id})
-	return err
-}
